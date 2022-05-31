3c06bb567a4422351def98e7f8e49b8b91c2741e
Nicolas Magro <anicomagro@gmail.com>
2022-05-31 16:24:00 -0300

    pasaje de las listas realizadas

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -14,7 +14,7 @@ for (int i = 0; i < cantTareas; i++)
 
 mostrarTarea(tareasPendientes);
 
-confirmar(tareasPendientes, tareasRealizadas);
+confirmar(tareasPendientes, tareasRealizadas, cantTareas);
 
 //Cuerpo de las funciones
 
@@ -28,16 +28,26 @@ void mostrarTarea(List<tareas> mostrar)
     }
 }
 
-void confirmar(List<tareas> tareasPendientes, List<tareas> tareasRealizadas)
+void confirmar(List<tareas> tareasPendientes, List<tareas> tareasRealizadas, int cantTareas)
 {
     int realizada;
     int cant = tareasPendientes.Count;
     Console.WriteLine($"Count: {cant}");
-    while (cant )
-    {
-        
-    }
-    for (int i = 0; i < cantTareas + 1; i++)
+    // for (int i = 0; i < cantTareas + 1; i++)
+    // {
+    //     realizada = 0;
+    //     Console.WriteLine("La tarea {0} fue realizada?", i+1);
+    //     Console.WriteLine("Ingrese 1 si esta realizada");
+    //     realizada = Convert.ToInt32(Console.ReadLine());
+    //     if (realizada == 1)
+    //     {
+    //         var tarea = tareasPendientes[i];
+    //         tareasRealizadas.Add(tarea);
+    //         tareasPendientes.RemoveAt(i);
+    //         Console.WriteLine("Tarea desplazada");
+    //     }
+    // }
+    for (int i = 0; cant != 0; i++)
     {
         realizada = 0;
         Console.WriteLine("La tarea {0} fue realizada?", i+1);
@@ -45,9 +55,12 @@ void confirmar(List<tareas> tareasPendientes, List<tareas> tareasRealizadas)
         realizada = Convert.ToInt32(Console.ReadLine());
         if (realizada == 1)
         {
-            tareasRealizadas.Add(new tareas() {tareaID = tareasPendientes[i].tareaID, descripcion = tareasPendientes[i].descripcion, duracion = tareasPendientes[i].duracion});
+            var tarea = tareasPendientes[i];
+            tareasRealizadas.Add(tarea);
             tareasPendientes.RemoveAt(i);
+            i--;
             Console.WriteLine("Tarea desplazada");
         }
+        cant--;
     }
 }
